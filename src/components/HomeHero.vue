2324b200f3ff07c7bb842bfb95f5ae0af15d654f
kazuo.kawai <kazuo.kawai@gmail.com>
2020-11-06 22:26:38 +0900

    initial commit

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,27 @@
+<template>
+   <section class="home-hero">
+       <v-container fluid fill-height class="home-hero__content">
+           <v-row>
+               <v-col class="home-hero__content-text">
+                   <p>Welcom to my website.</p>
+               </v-col>
+           </v-row>
+       </v-container>
+   </section> 
+</template>
+<style lang="scss" scoped>
+.home-hero__content {
+    background: url("../assets/logo.png");
+    background-size: cover;
+    background-position: center center;
+    width: 100%;
+    height: 50vh;
+
+    &-text {
+        color: red;
+        text-align: center;
+        font-size: 28px;
+        font-weight: bold;
+    }
+}
+</style>
