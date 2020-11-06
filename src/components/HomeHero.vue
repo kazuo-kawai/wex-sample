5cb08f42fcb36adf7df37f256aa99fad3cb28b41
kazuo.kawai <kazuo.kawai@gmail.com>
2020-11-06 23:20:33 +0900

    commit

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -3,7 +3,7 @@
        <v-container fluid fill-height class="home-hero__content">
            <v-row>
                <v-col class="home-hero__content-text">
-                   <p>Welcom to my website.</p>
+                   <p>Welcom to WEXLIVE</p>
                </v-col>
            </v-row>
        </v-container>
@@ -11,17 +11,25 @@
 </template>
 <style lang="scss" scoped>
 .home-hero__content {
-    background: url("../assets/logo.png");
+/*    background: url("../assets/logo.png");*/
     background-size: cover;
     background-position: center center;
     width: 100%;
     height: 50vh;
 
+    @include display_pc {
+        height: 100vh !important;
+    }
+
     &-text {
         color: red;
         text-align: center;
-        font-size: 28px;
+        font-size: 20px;
         font-weight: bold;
+
+        @include display_pc {
+            font-size: 40px !important;
+        }
     }
 }
 </style>
